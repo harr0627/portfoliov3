fa78de318aee55e6f129609574ccee47e5865e9c
harr0627 <harr0627@algonquinlive.com>
2022-03-13 09:41:26 -0400

    updated spacing for projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,13 +3,13 @@ import { FaGithub } from 'react-icons/fa';
 
 export default function Project({ img, title, desc, code, site }) {
   return (
-    <div className="grid gap-10 lg:grid-cols-2 xl:grid-cols-2 sm:grid-cols-1 auto-rows-fr">
-      <div className="grid place-content-center">
-        <img className="" src={img} alt={title} />
+    <div className="grid lg:gap-10 lg:grid-cols-2 xl:grid-cols-2 sm:grid-cols-1 auto-rows-fr">
+      <div className="grid place-content-center md:max-h-96">
+        <img className="md:max-h-96" src={img} alt={title} />
       </div>
       <div>
         <h3 className="text-center pb-3 font-bold">{title}</h3>
-        <p className="text-lg">{desc}</p>
+        <p className="text-lg  text-center ">{desc}</p>
         <div className="mt-10 grid gap-1 grid-cols-1 place-content-center">
           <a
             role="button"
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,13 +5,13 @@ import projects from '../assets/data.js';
 export default function Projects() {
   return (
     <>
-      <div id="projects" className=" pt-20">
+      <div id="projects" className="pt-20">
         <div className="container mx-auto max-w-6xl">
           <h2 className="text-4xl text-center ont-extrabold text-transparent bg-clip-text bg-gradient-to-r from-rose-600 via-purple-500 to-sky-500">
             Projects
           </h2>
           <div className="sm:p-2 mx-auto">
-            <div className="px-10 py-20 grid gap-10 lg:grid-cols-1 xl:grid-cols-1 sm:grid-cols-1 auto-rows-fr">
+            <div className="px-10 py-20 grid lg:grid-cols-1 lg:gap-20 xl:grid-cols-1 sm:grid-cols-1 auto-rows-fr">
               {projects.map((item) => (
                 <Project
                   key={item.id}
